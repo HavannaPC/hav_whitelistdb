722c18aa23201c539fb7249f7dac4dd5de9c918b
Jarrett B <jarrettboice@gmail.com>
2018-11-19 20:56:34 -0800

    Update

--- server.lua
+++ server.lua
@@ -1,90 +1,100 @@
--- Script made by Havanna (http://steamcommunity.com/id/HavannaPC/)
+-- Script made by Havanna maintained by itsJarrett
 
-AddEventHandler( "playerConnecting", function(name, setReason )
+AddEventHandler( 'playerConnecting', function(name, setReason )
 	local identifier = GetPlayerIdentifiers(source)[1]
-	if not isWhiteListed(identifier) then
-		setReason("You are not whitelisted on this server")
-		print("(" .. identifier .. ") " .. name .. " has been kicked because he is not WhiteListed")
+	local whitelistStatus = getWhitelisted(identifier)
+	if whitelistStatus == 0 then
+		setReason('You are not whitelisted on this server')
+		print('(' .. identifier .. ') ' .. name .. ' has been kicked because they are not WhiteListed')
 		CancelEvent()
-    end
+	elseif whitelistStatus == 2 then
+		setReason('You are banned!')
+		print('(' .. identifier .. ') ' .. name .. ' has been kicked because they are banned!')
+		CancelEvent()
+	end
 end)
 
 -- Chat Command to add someone in WhiteList
-TriggerEvent('es:addGroupCommand', 'wladd', "admin", function(source, args, user)
+TriggerEvent('es:addGroupCommand', 'wladd', 'admin', function(source, args, user)
 	if #args == 2 then
 		if isWhiteListed(args[2]) then
-			TriggerClientEvent('chatMessage', source, "SYSTEM", {255, 0, 0}, args[2] .. " is already whitelisted!")
+			TriggerClientEvent('chatMessage', source, 'SYSTEM', {255, 0, 0}, args[2] .. ' is already whitelisted!')
 		else
 			addWhiteList(args[2])
-			TriggerClientEvent('chatMessage', source, "SYSTEM", {255, 0, 0}, args[2] .. " has been whitelisted!")
+			TriggerClientEvent('chatMessage', source, 'SYSTEM', {255, 0, 0}, args[2] .. ' has been whitelisted!')
 		end
 	else
-		TriggerClientEvent('chatMessage', source, "SYSTEM", {255, 0, 0}, "Incorrect identifier!")
+		TriggerClientEvent('chatMessage', source, 'SYSTEM', {255, 0, 0}, 'Incorrect identifier!')
 	end
 end, function(source, args, user)
-	TriggerClientEvent('chatMessage', source, "SYSTEM", {255, 0, 0}, "Insufficienct permissions!")
+	TriggerClientEvent('chatMessage', source, 'SYSTEM', {255, 0, 0}, 'Insufficienct permissions!')
 end)
 
 -- Chat Command to remove someone in WhiteList
-TriggerEvent('es:addGroupCommand', 'wlremove', "admin", function(source, args, user)
+TriggerEvent('es:addGroupCommand', 'wlremove', 'admin', function(source, args, user)
 	if #args == 2 then
 		if isWhiteListed(args[2]) then
 			removeWhiteList(args[2])
-			TriggerClientEvent('chatMessage', source, "SYSTEM", {255, 0, 0}, args[2] .. " is no longer whitelisted!")
+			TriggerClientEvent('chatMessage', source, 'SYSTEM', {255, 0, 0}, args[2] .. ' is no longer whitelisted!')
 		else
-			TriggerClientEvent('chatMessage', source, "SYSTEM", {255, 0, 0}, args[2] .. " is not whitelisted from!")
+			TriggerClientEvent('chatMessage', source, 'SYSTEM', {255, 0, 0}, args[2] .. ' is not whitelisted from!')
 		end
 	else
-		TriggerClientEvent('chatMessage', source, "SYSTEM", {255, 0, 0}, "Incorrect identifier!")
+		TriggerClientEvent('chatMessage', source, 'SYSTEM', {255, 0, 0}, 'Incorrect identifier!')
 	end
 end, function(source, args, user)
-	TriggerClientEvent('chatMessage', source, "SYSTEM", {255, 0, 0}, "Insufficienct permissions!")
+	TriggerClientEvent('chatMessage', source, 'SYSTEM', {255, 0, 0}, 'Insufficienct permissions!')
 end)
 
 function addWhiteList(identifier)
-	MySQL.Sync.execute("INSERT INTO user_whitelist (`identifier`, `whitelisted`) VALUES (@identifier, @whitelisted)",{['@identifier'] = identifier, ['@whitelisted'] = 1})
+	MySQL.Sync.execute('INSERT INTO user_whitelist (`identifier`, `whitelisted`) VALUES (@identifier, @whitelisted)',{['@identifier'] = identifier, ['@whitelisted'] = 1})
 end
 
 function removeWhiteList(identifier)
-	MySQL.Sync.execute("DELETE FROM user_whitelist WHERE identifier = @identifier", {['@identifier'] = identifier})
+	MySQL.Sync.execute('DELETE FROM user_whitelist WHERE identifier = @identifier', {['@identifier'] = identifier})
 end
 
 function isWhiteListed(identifier)
-	local result = MySQL.Sync.fetchScalar("SELECT whitelisted FROM user_whitelist WHERE identifier = @username AND whitelisted = 1", {['@username'] = identifier})
+	local result = MySQL.Sync.fetchScalar('SELECT whitelisted FROM user_whitelist WHERE identifier = @username AND whitelisted = 1', {['@username'] = identifier})
 	if result then
 		return true
 	end
 	return false
 end
 
+function getWhitelisted(identifier)
+	local result = MySQL.Sync.fetchScalar('SELECT whitelisted FROM user_whitelist WHERE identifier = @username', {['@username'] = identifier})
+	return result
+end
+
 -- Rcon command to add/remove someone in WhiteList
 AddEventHandler('rconCommand', function(commandName, args)
 	if commandName == 'wladd' then
 		if #args ~= 1 then
-			RconPrint("Usage: whitelistadd [identifier]\n")
+			RconPrint('Usage: whitelistadd [identifier]\n')
 			CancelEvent()
 			return
 		end
 		if isWhiteListed(args[1]) then
-			RconPrint(args[1] .. " is already whitelisted!\n")
+			RconPrint(args[1] .. ' is already whitelisted!\n')
 			CancelEvent()
 		else
 			addWhiteList(args[1])
-			RconPrint(args[1] .. " has been whitelisted!\n")
+			RconPrint(args[1] .. ' has been whitelisted!\n')
 			CancelEvent()
 		end
 	elseif commandName == 'wlremove' then
 		if #args ~= 1 then
-			RconPrint("Usage: whitelistremove [identifier]\n")
+			RconPrint('Usage: whitelistremove [identifier]\n')
 			CancelEvent()
 			return
 		end
 		if isWhiteListed(args[1]) then
 			removeWhiteList(args[1])
-			RconPrint(args[1] .. " is no longer whitelisted!\n")
+			RconPrint(args[1] .. ' is no longer whitelisted!\n')
 			CancelEvent()
 		else
-			RconPrint(args[1] .. " is not whitelisted!\n")
+			RconPrint(args[1] .. ' is not whitelisted!\n')
 			CancelEvent()
 		end
 	end
