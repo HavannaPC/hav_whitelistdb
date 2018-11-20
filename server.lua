b8a9b5a3c8fcac4f63c1278686c3eb3243f90fc0
Jarrett B <jarrettboice@gmail.com>
2018-11-19 21:10:03 -0800

    Another formatting update

--- server.lua
+++ server.lua
@@ -14,13 +14,13 @@ AddEventHandler( 'playerConnecting', function(name, setReason )
 	end
 end)
 
--- Chat Command to add someone in WhiteList
+-- Chat Command to add someone in Whitelist
 TriggerEvent('es:addGroupCommand', 'wladd', 'admin', function(source, args, user)
 	if #args == 2 then
 		if isWhitelisted(args[2]) then
 			TriggerClientEvent('chatMessage', source, 'SYSTEM', {255, 0, 0}, args[2] .. ' is already whitelisted!')
 		else
-			addWhiteList(args[2])
+			addWhitelist(args[2])
 			TriggerClientEvent('chatMessage', source, 'SYSTEM', {255, 0, 0}, args[2] .. ' has been whitelisted!')
 		end
 	else
@@ -30,11 +30,11 @@ end, function(source, args, user)
 	TriggerClientEvent('chatMessage', source, 'SYSTEM', {255, 0, 0}, 'Insufficienct permissions!')
 end)
 
--- Chat Command to remove someone in WhiteList
+-- Chat Command to remove someone in Whitelist
 TriggerEvent('es:addGroupCommand', 'wlremove', 'admin', function(source, args, user)
 	if #args == 2 then
 		if isWhitelisted(args[2]) then
-			removeWhiteList(args[2])
+			removeWhitelist(args[2])
 			TriggerClientEvent('chatMessage', source, 'SYSTEM', {255, 0, 0}, args[2] .. ' is no longer whitelisted!')
 		else
 			TriggerClientEvent('chatMessage', source, 'SYSTEM', {255, 0, 0}, args[2] .. ' is not whitelisted from!')
@@ -46,11 +46,11 @@ end, function(source, args, user)
 	TriggerClientEvent('chatMessage', source, 'SYSTEM', {255, 0, 0}, 'Insufficienct permissions!')
 end)
 
-function addWhiteList(identifier)
+function addWhitelist(identifier)
 	MySQL.Sync.execute('INSERT INTO user_whitelist (`identifier`, `whitelisted`) VALUES (@identifier, @whitelisted)',{['@identifier'] = identifier, ['@whitelisted'] = 1})
 end
 
-function removeWhiteList(identifier)
+function removeWhitelist(identifier)
 	MySQL.Sync.execute('DELETE FROM user_whitelist WHERE identifier = @identifier', {['@identifier'] = identifier})
 end
 
@@ -68,7 +68,7 @@ function getWhitelisted(identifier)
 	return result
 end
 
--- Rcon command to add/remove someone in WhiteList
+-- Rcon command to add/remove someone in Whitelist
 AddEventHandler('rconCommand', function(commandName, args)
 	if commandName == 'wladd' then
 		if #args ~= 1 then
@@ -80,7 +80,7 @@ AddEventHandler('rconCommand', function(commandName, args)
 			RconPrint(args[1] .. ' is already whitelisted!\n')
 			CancelEvent()
 		else
-			addWhiteList(args[1])
+			addWhitelist(args[1])
 			RconPrint(args[1] .. ' has been whitelisted!\n')
 			CancelEvent()
 		end
@@ -91,7 +91,7 @@ AddEventHandler('rconCommand', function(commandName, args)
 			return
 		end
 		if isWhitelisted(args[1]) then
-			removeWhiteList(args[1])
+			removeWhitelist(args[1])
 			RconPrint(args[1] .. ' is no longer whitelisted!\n')
 			CancelEvent()
 		else
